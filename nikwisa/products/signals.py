713cf6bf2ad6017583b0a90ed8da796b70fb1ed4
Bulemu Shilukukwa <narcoa47@gmail.com>
2024-12-10 19:06:29 +0200

    more-changes

--- nikwisa/products/signals.py
+++ nikwisa/products/signals.py
@@ -2,9 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
 from .models import CentralizedProduct
-from products.models import YourProductModel  # Replace with your actual product model
+from products.models import  CentralizedProduct # Replace with your actual product model
 
-@receiver(post_save, sender=YourProductModel)
+@receiver(post_save, sender=CentralizedProduct)
 def create_centralized_product(sender, instance, created, **kwargs):
     if created:
         content_type = ContentType.objects.get_for_model(instance)
