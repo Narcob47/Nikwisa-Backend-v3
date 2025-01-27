3daa56835763e6d6350ab1ace97e7ffa9cb03d84
jabulani-creator <charingabiggie@gmail.com>
2025-01-27 21:56:42 +0200

    added event subcategories

--- nikwisa/event_planning/views.py
+++ nikwisa/event_planning/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import EventPlanningCategories, EventPlanningSubCategory
 from .serializers import EventPlanningCategoriesSerializer, EventPlanningSubCategorySerializer
+
 
 class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
     queryset = EventPlanningCategories.objects.all()
@@ -13,8 +13,15 @@ class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
     search_fields = ['title']
     ordering_fields = ['title']
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        subcategory_id = self.request.query_params.get('subcategory_id', None)
+        if subcategory_id:
+            queryset = queryset.filter(subcategories__id=subcategory_id)
+        return queryset
+
     def create(self, request):
-        serializer = EventPlanningCategoriesSerializer(data=request.data)
+        serializer = EventPlanningCategoriesSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -24,7 +31,7 @@ class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
             category = EventPlanningCategories.objects.get(pk=pk)
         except EventPlanningCategories.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = EventPlanningCategoriesSerializer(category)
+        serializer = EventPlanningCategoriesSerializer(category, context={'request': request})
         return Response(serializer.data)
 
     def update(self, request, pk=None):
@@ -32,7 +39,7 @@ class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
             category = EventPlanningCategories.objects.get(pk=pk)
         except EventPlanningCategories.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = EventPlanningCategoriesSerializer(category, data=request.data)
+        serializer = EventPlanningCategoriesSerializer(category, data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
@@ -42,7 +49,7 @@ class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
             category = EventPlanningCategories.objects.get(pk=pk)
         except EventPlanningCategories.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = EventPlanningCategoriesSerializer(category, data=request.data, partial=True)
+        serializer = EventPlanningCategoriesSerializer(category, data=request.data, partial=True, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
@@ -55,14 +62,17 @@ class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
         category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class EventPlanningSubCategoryViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = EventPlanningSubCategory.objects.all()
-        serializer = EventPlanningSubCategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+
+class EventPlanningSubCategoryViewSet(viewsets.ModelViewSet):
+    queryset = EventPlanningSubCategory.objects.all()
+    serializer_class = EventPlanningSubCategorySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['categories__id', 'categories__title']
+    search_fields = ['title']
+    ordering_fields = ['title']
 
     def create(self, request):
-        serializer = EventPlanningSubCategorySerializer(data=request.data)
+        serializer = EventPlanningSubCategorySerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -72,7 +82,17 @@ class EventPlanningSubCategoryViewSet(viewsets.ViewSet):
             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
         except EventPlanningSubCategory.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = EventPlanningSubCategorySerializer(subcategory)
+        serializer = EventPlanningSubCategorySerializer(subcategory, context={'request': request})
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+        except EventPlanningSubCategory.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = EventPlanningSubCategorySerializer(subcategory, data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
 
     def partial_update(self, request, pk=None):
@@ -80,7 +100,8 @@ class EventPlanningSubCategoryViewSet(viewsets.ViewSet):
             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
         except EventPlanningSubCategory.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = EventPlanningSubCategorySerializer(subcategory, data=request.data, partial=True)
+        serializer = EventPlanningSubCategorySerializer(subcategory, data=request.data, partial=True, context={'request': request})
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
@@ -91,4 +112,212 @@ class EventPlanningSubCategoryViewSet(viewsets.ViewSet):
             return Response(status=status.HTTP_404_NOT_FOUND)
         subcategory.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
+#     queryset = EventPlanningCategories.objects.all()
+#     serializer_class = EventPlanningCategoriesSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+#     filterset_fields = ['title']
+#     search_fields = ['title']
+#     ordering_fields = ['title']
+    
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         subcategory_id = self.request.query_params.get('subcategory_id', None)
+#         if subcategory_id:
+#             # Filter categories by the related subcategory ID
+#             queryset = queryset.filter(subcategories__id=subcategory_id)
+#         return queryset
+    
+
+    
+#     def create(self, request):
+#         serializer = EventPlanningCategoriesSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#     def retrieve(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningCategoriesSerializer(category)
+#         return Response(serializer.data)
+
+#     def update(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningCategoriesSerializer(category, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def partial_update(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningCategoriesSerializer(category, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def destroy(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class EventPlanningSubCategoryViewSet(viewsets.ModelViewSet):
+#     queryset = EventPlanningSubCategory.objects.all()
+#     serializer_class = EventPlanningSubCategorySerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+#     filterset_fields = ['categories__id', 'categories__title']  # Allow filtering by parent category
+#     search_fields = ['title']  # Enable search by title
+#     ordering_fields = ['title']  # Enable ordering by title
+
+#     def create(self, request):
+#         serializer = EventPlanningSubCategorySerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#     def retrieve(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningSubCategorySerializer(subcategory)
+#         return Response(serializer.data)
+
+#     def update(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningSubCategorySerializer(subcategory, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def partial_update(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningSubCategorySerializer(subcategory, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def destroy(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         subcategory.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# from django.shortcuts import render
+# from rest_framework import viewsets, status, filters
+# from rest_framework.response import Response
+# from django_filters.rest_framework import DjangoFilterBackend
+# from .models import EventPlanningCategories, EventPlanningSubCategory
+# from .serializers import EventPlanningCategoriesSerializer, EventPlanningSubCategorySerializer
+
+# class EventPlanningCategoriesViewSet(viewsets.ModelViewSet):
+#     queryset = EventPlanningCategories.objects.all()
+#     serializer_class = EventPlanningCategoriesSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+#     filterset_fields = ['title']
+#     search_fields = ['title']
+#     ordering_fields = ['title']
+
+#     def create(self, request):
+#         serializer = EventPlanningCategoriesSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#     def retrieve(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningCategoriesSerializer(category)
+#         return Response(serializer.data)
+
+#     def update(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningCategoriesSerializer(category, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def partial_update(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningCategoriesSerializer(category, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def destroy(self, request, pk=None):
+#         try:
+#             category = EventPlanningCategories.objects.get(pk=pk)
+#         except EventPlanningCategories.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# class EventPlanningSubCategoryViewSet(viewsets.ViewSet):
+#     def list(self, request):
+#         queryset = EventPlanningSubCategory.objects.all()
+#         serializer = EventPlanningSubCategorySerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = EventPlanningSubCategorySerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#     def retrieve(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningSubCategorySerializer(subcategory)
+#         return Response(serializer.data)
+
+#     def partial_update(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = EventPlanningSubCategorySerializer(subcategory, data=request.data, partial=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def destroy(self, request, pk=None):
+#         try:
+#             subcategory = EventPlanningSubCategory.objects.get(pk=pk)
+#         except EventPlanningSubCategory.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         subcategory.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
